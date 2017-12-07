17d1644219431aef554bc6e47f31be4c7a257f31
t541817 <t541817@tamu.edu>
2017-12-07 02:00:11 -0600

    adding Dockerfile, .yml, and requirements.txt. also cli.py placeholder

---
+++ CLI.py
@@ -0,0 +1,19 @@
+from subprocess import call
+import sys
+import requests
+
+call(["" , "-l"])
+'''
+print "this is the name of the script: ", sys.argv[0]
+
+if sys.argv[1] == "md5"
+	curl localhost:5000/md5/
+
+elif sys.argv[1]
+
+elif sys.argv[1]
+
+elif sys.argv[1]
+
+elif sys.argv[1]
+'''
